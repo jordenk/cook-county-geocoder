3532f3bd6c098512f8429b9119274c2410f2ca4d
jordenk <jordenkawamoto@gmail.com>
2021-03-09 08:03:40 -0600

    Index addresses in local ElasticSearch cluster.

---
+++ data/index.go
@@ -0,0 +1,118 @@
+package data
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"github.com/cenkalti/backoff/v4"
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esutil"
+	"log"
+	"sync/atomic"
+	"time"
+)
+
+// Receive normalized structs and index ES.
+
+func BuildEsClient() *elasticsearch.Client {
+	retryBackoff := backoff.NewExponentialBackOff()
+
+	es, err := elasticsearch.NewClient(elasticsearch.Config{
+		RetryOnStatus: []int{502, 503, 504, 429},
+		RetryBackoff: func(i int) time.Duration {
+			if i == 1 {
+				retryBackoff.Reset()
+			}
+			return retryBackoff.NextBackOff()
+		},
+		MaxRetries: 5,
+	})
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+	return es
+}
+
+// TODO create index if it doesn't exist.
+func HandleIndex() {
+	// Check if index exists
+
+	//res, err = es.Indices.Create(indexName)
+	//if err != nil {
+	//	log.Fatalf("Cannot create index: %s", err)
+	//}
+	//if res.IsError() {
+	//	log.Fatalf("Cannot create index: %s", res)
+	//}
+	//res.Body.Close()
+}
+
+const (
+	WORKERS = 20
+)
+
+func BulkIndexEs(es *elasticsearch.Client, indexName string, esAddresses *[]EsAddress) {
+	bulkIndexer, err := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+		Index:      indexName,
+		Client:     es,
+		NumWorkers: WORKERS,
+	})
+	if err != nil {
+		log.Fatalf("Error creating the indexer: %s", err)
+	}
+
+	start := time.Now().UTC()
+	var countSuccessful uint64
+
+	for _, esAddress := range *esAddresses {
+		// Encode article to JSON
+		data, err := json.Marshal(esAddress)
+		if err != nil {
+			log.Fatalf("Cannot encode address document %v: %s", esAddress, err)
+		}
+
+		err = bulkIndexer.Add(
+			context.Background(),
+			esutil.BulkIndexerItem{
+				Action: "index",
+				Body:   bytes.NewReader(data),
+				OnSuccess: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem) {
+					atomic.AddUint64(&countSuccessful, 1)
+				},
+				OnFailure: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem, err error) {
+					if err != nil {
+						log.Printf("ERROR: %s", err)
+					} else {
+						log.Printf("ERROR: %s: %s", res.Error.Type, res.Error.Reason)
+					}
+				},
+			},
+		)
+		if err != nil {
+			log.Fatalf("Unexpected error: %s", err)
+		}
+	}
+
+	if err := bulkIndexer.Close(context.Background()); err != nil {
+		log.Fatalf("Unexpected error: %s", err)
+	}
+
+	biStats := bulkIndexer.Stats()
+	dur := time.Since(start)
+	if biStats.NumFailed > 0 {
+		log.Fatalf(
+			"Indexed [%d] documents with [%d] errors in %s (%d docs/sec)",
+			int64(biStats.NumFlushed),
+			int64(biStats.NumFailed),
+			dur.Truncate(time.Millisecond),
+			int64(1000.0/float64(dur/time.Millisecond)*float64(biStats.NumFlushed)),
+		)
+	} else {
+		log.Printf(
+			"Sucessfuly indexed [%d] documents in %s (%d docs/sec)",
+			int64(biStats.NumFlushed),
+			dur.Truncate(time.Millisecond),
+			int64(1000.0/float64(dur/time.Millisecond)*float64(biStats.NumFlushed)),
+		)
+	}
+}
